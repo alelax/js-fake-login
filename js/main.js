323bf4c3b9b639e0a567919c696c0bf1457707cd
Giangio96 <95866734+Giangio96@users.noreply.github.com>
2022-03-18 10:50:04 +0100

    classe-Create-Utente

---
+++ js/main.js
@@ -0,0 +1,10 @@
+//variabili da utilizzare
+//chiamata all API per return utenti
+//Comparazione (if)
+//Simualzione cambio page / messaggio di errore 
+//definire classe utente
+/*"id": 1,
+    "name": "Leanne Graham",
+    "username": "Bret",
+    "email": "Sincere@april.biz",*/
+
---
+++ js/user.js
@@ -0,0 +1,22 @@
+class User {
+    #id;
+    #name;
+    #username;
+    #email;
+    #password = "PippoBaudo";
+
+    constructor(id, name, username, email) {
+        this.#id = id;
+        this.#name = name;
+        this.#username = username;
+        this.#email = email;
+    }
+
+    get username() {
+        return this.#username;
+    }
+    set username(nuovoUsername) {
+        this.#username = nuovoUsername;
+    }
+
+}
