2c589acd360e7c1c7d7dfe8372d9810ff0c125db
Ionut Asaftei <ionut.asaftei2014@gmail.com>
2022-03-18 12:42:38 +0100

    create logout function

--- js/main.js
+++ js/main.js
@@ -11,6 +11,7 @@
 import  {User}  from "./user.js";
 
 document.querySelector('#login-btn').addEventListener('click', login);
+document.querySelector("#logout-btn").addEventListener('click', logOut);
 
 async function login() {
     const formUserName = document.getElementById('form-username').value;
@@ -25,10 +26,34 @@ async function login() {
         const fetchedUsers = users.map(user => new User(user.id, user.name, user.username, user.email));
         const foundUser = fetchedUsers.find(user => user.username === formUserName && user.password === formUserPassword);
         console.log(foundUser);
+        if(foundUser){
+           toogleToDoList(true)
+        }
     } catch (err) {
         console.log(err);
     }
+
 }
+
+
+function logOut(){
+    toogleToDoList(false)
+}
+
+function toogleToDoList(showTodoList){
+     document.querySelector(".main").style.display = showTodoList ? "none" :"block";
+     document.querySelector(".main-user-logged").style.display = showTodoList ? "block" :"none";
+     
+   /*  if(showTodoList){
+        document.querySelector(".main").style.display="none"
+            document.querySelector(".main-user-logged").style.display="block"
+        }else{
+             document.querySelector(".main").style.display="block"
+             document.querySelector(".main-user-logged").style.display="none"
+        } */
+    }
+
+
 
 
 
